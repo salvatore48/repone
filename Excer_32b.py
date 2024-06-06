0defb1e0296840ba1b057aac8ee79c74c8532c50
salvatore48 <89104250+salvatore48@users.noreply.github.com>
2024-06-06 16:18:20 -0600

    Add files via upload

---
+++ Excer_32b.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+sh = input("Enter hours: ")
+sr = input("Enter rate: ")
+fh = float(sh)
+fr = float(sr)
+#print(fh,fr)
+if fh > 40 :
+	print("Overtime")
+	reg = fr * fh
+	otp = (fh - 40) * (fr * 0.5)
+	print (reg,otp)
+	xp = reg + otp
+else:
+	print("Regular")
+	xp = fh * fr
+print("Pay: ",xp)
